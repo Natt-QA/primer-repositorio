f36f14075664b228d1dfc4c257373219073e1d38
Natt-QA <larrondo.nat@gmail.com>
2026-05-02 20:18:51 -0300

    first commit

---
+++ src/principal/Principal.java
@@ -0,0 +1,12 @@
+package principal;
+
+public class Principal {
+    public static void main(String[] args) {
+        //Primer linea escrita de codigo
+        System.out.println("Hello world");
+        System.out.println("New code");
+
+        System.out.print("This");
+        System.out.print(" is a print test");
+    }
+}
